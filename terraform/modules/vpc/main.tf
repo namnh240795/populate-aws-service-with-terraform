977898c7dfebdbb1f890d8209c00abf22bec5a17
namnh240795 <namnh240795@gmail.com>
2023-06-27 00:25:58 +0700

    Provision ec2 with terraform successfully

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -62,3 +62,66 @@ resource "aws_route_table_association" "public" {
     count = length(var.public_subnets)
 }
 
+resource "aws_key_pair" "ec2_key_pem" {
+    key_name   = "${var.vpc_name}-${var.environment}-ec2-key"
+    public_key = file("../.ssh/aws_tf.pub")
+}
+
+resource "aws_security_group" "ec2_to_internet" {
+    name        = "${var.vpc_name}-${var.environment}-allow-http"
+    description = "allow everything"
+    vpc_id      = aws_vpc.main.id
+
+    ingress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+}
+
+data "aws_ec2_instance_type" "t2_micro" {
+    instance_type = "t2.micro"
+}
+
+data "aws_ami" "aws_ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+// Create EC2 instance
+resource "aws_instance" "ec2" {
+    ami = data.aws_ami.aws_ubuntu.id
+    instance_type = data.aws_ec2_instance_type.t2_micro.id
+    key_name = aws_key_pair.ec2_key_pem.key_name
+    vpc_security_group_ids = [aws_security_group.ec2_to_internet.id]
+    subnet_id = element(aws_subnet.public.*.id, 0)
+    associate_public_ip_address = true
+    tags = {
+        Name = "${var.vpc_name}-${var.environment}-ec2"
+        Environment = var.environment
+    }
+}
